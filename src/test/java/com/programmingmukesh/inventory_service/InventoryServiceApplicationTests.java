0bbb75a60df6159da741c47d6c5087616d0213ed
Mukesh Kumar <Mukeshkumar@cloudprism.in>
2024-10-28 11:58:47 +0530

    initialise springboot and connect db

---
+++ src/test/java/com/programmingmukesh/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingmukesh.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
